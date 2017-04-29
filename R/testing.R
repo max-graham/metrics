3e5a23079d7e126849f11e2bc51109a68117ec27
Max Graham <maxgraham@outlook.com>
2017-04-28 23:18:37 -0300

    Initial commit.

---
+++ R/testing.R
@@ -0,0 +1,13 @@
+
+testing <- function(x){
+  #' testing
+  #'
+  #' This is a test function in my test package.
+  #' @param x the input
+  #' @return the output
+  #' @examples
+  #' testing("Anything!")
+  #' @export
+  
+  writeLines("Success!")
+}
